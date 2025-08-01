70a34734e3fe78c43dd9d1f04c176c5b522db999
Ap-Developer-18 <puniaaman369@gmail.com>
2025-08-01 16:42:09 +0530

    start the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import { ThemeProvider } from "./context/ThemeContext";
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider>
-      <Router>
-        <div className="min-h-screen transition-all bg-background text-foreground">
-          <Header />
-          <main className="pt-20">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </ThemeProvider>
+    <>
+      <div className=""></div>
+    </>
   );
 };
 
